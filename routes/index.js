f6bafcde23851c7fa9f255cddfe6fd90c3b132c0
Edward Legarda <elegarda@sitesas.co>
2020-01-15 17:14:33 -0500

    final commit

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,35 @@
 'use strict';
 const express = require('express');
-const api	= express.Router();
 const productController = require('../controllers/productController');
+const userController = require('../controllers/userController');
+const auth = require('../middlewares/auth');
+const api	= express.Router();
 
-api.get('/products',productController.getProducts);
+api.get('/product', auth, productController.getProducts);
 api.get('/product/:productId', productController.getProduct);
-api.post('/product', productController.saveProduct);
-api.put('/product/:productId', productController.updateProduct);
-api.delete('/product/:productId', productController.deleteProduct);
+api.post('/product', auth, productController.saveProduct);
+api.put('/product/:productId', auth, productController.updateProduct);
+api.delete('/product/:productId', auth, productController.deleteProduct);
+api.post('/signup', userController.signUp);
+api.post('/signin', userController.signIn);
+api.get('/private', auth, function (req, res ){
+	res.status(200).send({ message : 'Tienes acceso'});
+})
 // pruebas
 
-api.get('/productsAll',productController.getAllProducts);
+api.get('/productsAll', auth, productController.getAllProducts);
 module.exports = api;
+
+/*
+
+SELECT nspname || '.' || relname AS "tabla",
+      pg_size_pretty(pg_total_relation_size(c.oid)) AS "tamanio"
+FROM  pg_class c
+LEFT JOIN pg_namespace n ON (n.oid = c.relnamespace)
+WHERE  n.nspname NOT IN ('pg_catalog', 'information_schema') AND
+      c.relkind <> 'i' AND
+      n.nspname !~ '^pg_toast'
+ORDER BY  pg_total_relation_size(c.oid) DESC
+LIMIT 20;
+
+*/
